e985b422bc2ee6712aaca6611452b3b8ed92eca5
jake <jake.quade@lightspeedhq.com>
2021-11-21 18:15:57 +1100

    got initial menu working

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,64 @@
 extern crate gio;
 extern crate gtk;
 
-use std::env;
+mod toolbar;
 
 use gtk::prelude::*;
-use gtk::{
-    Application,
-    ApplicationWindow,
-};
+use gtk::{Application, ApplicationWindow};
 
-fn main() {
-    let application = Application::builder().application_id("com.example.Rusic").build();
+use toolbar::MusicToolbar;
 
-    application.connect_activate(|application| {
-        let window = ApplicationWindow::builder().application(application).title("Rusic").build();
+struct App {
+    toolbar: MusicToolbar,
+    window: ApplicationWindow,
+}
+
+impl App {
+    fn new(application: &Application) -> Self {
+        let window = ApplicationWindow::builder()
+            .application(application)
+            .title("Rusic")
+            .build();
+
+        let toolbar = MusicToolbar::new();
+        window.add(toolbar.toolbar());
 
         window.show_all();
+
+        let app = App { toolbar, window };
+
+        app.connect_events();
+
+        app
+    }
+
+    fn connect_events(&self) {}
+
+    pub fn connect_toolbar_events(&self) {
+        let window = self.window.clone();
+        self.toolbar.quit_button.connect_clicked(move |_| unsafe {
+            window.destroy();
+        });
+
+        let play_button = self.toolbar.play_button.clone();
+        self.toolbar.play_button.connect_clicked(move |_| {
+            // if play_button.fmt() == Some(PLAY_STOCK.to_string()) {
+            //     play_button.set_stock_id(PAUSE_STOCK);
+            // } else {
+            //     play_button.set_stock_id(PLAY_STOCK);
+            // }
+            println!("{:?}", play_button);
+        });
+    }
+}
+
+fn main() {
+    let application = Application::builder()
+        .application_id("com.example.Rusic")
+        .build();
+
+    application.connect_activate(|application| {
+        let app = App::new(application);
     });
 
     application.run();
